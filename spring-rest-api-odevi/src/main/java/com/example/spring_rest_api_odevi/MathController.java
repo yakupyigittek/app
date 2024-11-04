b5301abf3373e8f666131a16b8320f526d21e3c4
yakupyigittek <yakupyigittek@gmail.com>
2024-11-04 22:15:02 +0300

    POST isteği yapan endpoint
    Verilen sayıların çarpımını yazdıran endpoint.

--- spring-rest-api-odevi/src/main/java/com/example/spring_rest_api_odevi/MathController.java
+++ spring-rest-api-odevi/src/main/java/com/example/spring_rest_api_odevi/MathController.java
@@ -1,7 +1,11 @@
 package com.example.spring_rest_api_odevi;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +19,15 @@ public class MathController {
             @RequestParam double sayi1,
             @RequestParam double sayi2) {
         double sonuc = sayi1 + sayi2;
+        return ResponseEntity.ok(sonuc);
+    }
+	
+	@PostMapping("/carp")
+    public ResponseEntity<Double> carp(@RequestBody List<Double> sayilar) {
+        double sonuc = 1;
+        for (double sayi : sayilar) {
+            sonuc *= sayi;
+        }
         return ResponseEntity.ok(sonuc);
     }
 }
