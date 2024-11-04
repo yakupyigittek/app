cdfb1a168447a2e1c24b8b2001d6976c1c0bd786
yakupyigittek <yakupyigittek@gmail.com>
2024-11-04 21:58:40 +0300

    GET isteği yapan endpoint
    Verilen "2" sayıyı toplayan ve sonucunu yazdıran endpoint.

--- spring-rest-api-odevi/src/main/java/com/example/spring_rest_api_odevi/MathController.java
+++ spring-rest-api-odevi/src/main/java/com/example/spring_rest_api_odevi/MathController.java
@@ -1,10 +1,20 @@
 package com.example.spring_rest_api_odevi;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/math")
 public class MathController {
-
+	
+	@GetMapping("/topla")
+    public ResponseEntity<Double> topla(
+            @RequestParam double sayi1,
+            @RequestParam double sayi2) {
+        double sonuc = sayi1 + sayi2;
+        return ResponseEntity.ok(sonuc);
+    }
 }
